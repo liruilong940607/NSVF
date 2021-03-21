8ccd0ffa26e472cce7cd6dc729d4c0a470a33a93
Ruilong Li <397653553@qq.com>
2021-03-20 17:24:07 -0700

    upgrade requirments, add training script for syn and TT

---
+++ train.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DATA=$1
+DATASET=/home/ICT2000/rli/data/nsvf_data/Synthetic_NeRF/$DATA
+SAVE=/home/ICT2000/rli/projects/nsvf_ckpt/$DATA
+
+python -u train.py ${DATASET} \
+    --user-dir fairnr \
+    --task single_object_rendering \
+    --train-views "0..100" --view-resolution "800x800" \
+    --max-sentences 1 --view-per-batch 4 --pixel-per-view 2048 \
+    --no-preload \
+    --sampling-on-mask 1.0 --no-sampling-at-reader \
+    --valid-views "100..200" --valid-view-resolution "400x400" \
+    --valid-view-per-batch 1 \
+    --transparent-background "1.0,1.0,1.0" --background-stop-gradient \
+    --arch nsvf_base \
+    --initial-boundingbox ${DATASET}/bbox.txt \
+    --use-octree \
+    --raymarching-stepsize-ratio 0.125 \
+    --discrete-regularization \
+    --color-weight 128.0 --alpha-weight 1.0 \
+    --optimizer "adam" --adam-betas "(0.9, 0.999)" \
+    --lr 0.001 --lr-scheduler "polynomial_decay" --total-num-update 150000 \
+    --criterion "srn_loss" --clip-norm 0.0 \
+    --num-workers 0 \
+    --seed 2 \
+    --save-interval-updates 500 --max-update 150000 \
+    --virtual-epoch-steps 5000 --save-interval 1 \
+    --half-voxel-size-at  "5000,25000,75000" \
+    --reduce-step-size-at "5000,25000,75000" \
+    --pruning-every-steps 2500 \
+    --keep-interval-updates 5 --keep-last-epochs 5 \
+    --log-format simple --log-interval 1 \
+    --save-dir ${SAVE} \
+    --tensorboard-logdir ${SAVE}/tensorboard \
+    | tee -a $SAVE/train.log
+
